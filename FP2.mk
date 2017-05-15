f2ebd7012f2553d4e2d1b0e46d26044a8f15da46
chrmhoffmann <chrmhoffmann@gmail.com>
2017-05-15 23:16:47 +0200

    FP2: more stuff

--- FP2.mk
+++ FP2.mk
@@ -195,9 +195,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.whitelist=/system/etc/whitelist_appops.xml
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
-
 PRODUCT_COPY_FILES += \
     device/fairphone/FP2/whitelist_appops.xml:system/etc/whitelist_appops.xml
 
